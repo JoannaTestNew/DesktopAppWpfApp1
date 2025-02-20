f02bb8350d97f5601f431b3c2eae8e09e987e558
Joanna Zheng <zhenglin880802@outlook.com>
2025-02-20 02:53:33 +0000

    Add project files.
    Add using directive for System.Windows in AssemblyInfo.cs
    
    This change introduces a `using System.Windows;` directive to the `AssemblyInfo.cs` file. This addition enables the use of types and members from the `System.Windows` namespace, which is essential for WPF applications. While it does not modify existing assembly attributes, it sets the stage for future enhancements that may involve WPF resources or features. []. &"

---
+++ DesktopAppWpfApp1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DesktopAppWpfApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    // No additional code needed for basic App.xaml.cs functionality
+}
+
